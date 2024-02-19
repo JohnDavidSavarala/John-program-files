75441a72b54c1dcc9e881073e6019b190ddfcfc4
JohnDavidSavarala <158747410+JohnDavidSavarala@users.noreply.github.com>
2024-02-19 14:44:06 +0530

    Update testPrime.c

--- testPrime.c
+++ testPrime.c
@@ -4,18 +4,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXARRAYLENGTH
 #include "prime.c"
 
 int main() 
 {
     int counter, passCounter = 0;
-    char testCases[MAXARRAYLENGTH][10] = {"5", "2", "3", "11", "23"};
-    int testCasesSize = 5; // Corrected size
+    char testCases[5][10] = {"5", "2", "3", "0", "John"};
+    int testCasesSize = 5; 
 
     for (counter = 0; counter < testCasesSize; counter++) 
     {
-        if (isPrime(testCases[counter]) != 0) // Check if return value is 0
+        if (isPrime(testCases[counter]) == 2) 
         {
             printf("Failed(%s)\n", testCases[counter]);
         }
